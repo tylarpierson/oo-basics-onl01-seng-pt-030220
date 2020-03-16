ab4c1ef8829ff8d146e9193a36b28a28a511ddc0
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-16 17:51:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -29,10 +29,7 @@ class Shoe
   def condition
     @condition
   end
-  def cobble=(cobble)
-    @cobble = cobble 
-  end 
-  def cobble 
-    @cobble
+  def cobble
+    puts "Your shoe is as good as new!"
   end 
 end 
